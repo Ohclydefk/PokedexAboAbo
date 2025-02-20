575640a9e1d492df4cc004b874037128a6810cb2
Ohclydefk <janclyde21@gmail.com>
2025-02-20 20:50:02 +0800

    Update server.js

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "../public")));
 app.use("/", pokemonRouter);
 
-const PORT = 8000 || process.env.PORT;
+const PORT = 8080 || process.env.PORT;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port localhost:${PORT}!`)
